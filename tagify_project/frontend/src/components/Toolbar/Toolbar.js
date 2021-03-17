028f275d69ed1e1d84107839893a5cbdaf8508c5
Krayonn <kris-beckers@hotmail.com>
2021-03-17 20:45:34 +0000

    ok

--- tagify_project/frontend/src/components/Toolbar/Toolbar.js
+++ tagify_project/frontend/src/components/Toolbar/Toolbar.js
@@ -10,7 +10,7 @@ const scopes = ['user-library-read','playlist-modify-private'];
 
 const toolbar = ( props ) => {
     const authHandler = () => {
-        const redirect = 'http://tagify.eu.pythonanywhere.com'
+        const redirect = 'https://tagify.eu.pythonanywhere.com'
         // const redirect = 'http://localhost:8000'
         // const redirect = 'http://localhost:8080'
         window.location.assign('https://accounts.spotify.com/authorize?client_id=803253b42a634b1ca7f1ce3cbd115b91&response_type=code&redirect_uri='+redirect+'&scope=' + scopes.join('%20') + '&state=oipxpoizxhpx');
