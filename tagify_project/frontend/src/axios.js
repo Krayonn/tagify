baa73ce16c94d895485329aab5e40ad64674387b
Krayonn <kris-beckers@hotmail.com>
2021-03-20 20:09:35 +0000

    removed logs

--- tagify_project/frontend/src/axios.js
+++ tagify_project/frontend/src/axios.js
@@ -2,14 +2,8 @@ import axios from 'axios';
 
 const baseUrl = process.env.API_URL;
 
-const test = () => {
-    console.log('BASEURL=',process.env.API_URL)
-    return process.env.API_URL
-}
 const instance = axios.create({
-    // baseURL: 'http://localhost:8000/'
-    // baseURL: "https://tagify.eu.pythonanywhere.com"
-    baseURL: test()
+    baseURL: process.env.API_URL
 });
 
 
